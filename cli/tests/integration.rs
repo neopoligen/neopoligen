30094f699c07b63c917f89639563bbe0e650e32c
Alan Smith <commit@alanwsmith.com>
2024-02-25 16:42:53 -0500

    Starting to add templates in test

--- cli/tests/integration.rs
+++ cli/tests/integration.rs
@@ -3,15 +3,19 @@ mod integration {
     use neopoligen_cli::site::Site;
     use pretty_assertions::assert_eq;
 
-    #[test]
-    pub fn basic_test() {
-        let site = Site::site1();
-        let mut env = Environment::new();
+    fn load_templates(env: &mut Environment) {
         env.add_template_owned(
             "pages/post/published.jinja",
             "This is the published post page".to_string(),
         )
         .unwrap();
+    }
+
+    #[test]
+    fn basic_test() {
+        let site = Site::site1();
+        let mut env = Environment::new();
+        load_templates(&mut env);
         let skeleton = env.get_template("pages/post/published.jinja").unwrap();
         let left = "This is the published post page".to_string();
         let right = skeleton.render(context!(name => "World")).unwrap();
