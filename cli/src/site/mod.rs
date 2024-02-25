7f67d733316468c5fb25b7c9cec886ec1d43a719
Alan Smith <commit@alanwsmith.com>
2024-02-24 21:40:58 -0500

    Getting initial page title

--- cli/src/site/mod.rs
+++ cli/src/site/mod.rs
@@ -1,6 +1,10 @@
 pub mod mocks;
 
+use crate::child::Child;
 use crate::page::Page;
+use crate::section::Section;
+use crate::section_category::SectionCategory;
+use crate::span::Span;
 use std::collections::BTreeMap;
 use std::sync::Mutex;
 
@@ -10,12 +14,64 @@ pub struct Site {
 }
 
 impl Site {
-    pub fn page_title(&self, id: &str) -> &str {
+    pub fn page_title(&self, id: &str) -> String {
+        let title = match self.pages.get(id) {
+            Some(page) => match get_title_section_title(&page.ast) {
+                Some(t) => t,
+                None => "Untitled".to_string(),
+            },
+            None => "(missing page)".to_string(),
+        };
+        title
+        //get_title_section_title(&self.pages.)
+
         // let mut cache = self.cache.lock().unwrap();
         // match cache.get("page_title") {
         //     Some(c) => {}
         //     None => {}
         // }
-        "Site 1 Home Page"
     }
+}
+
+fn filter_section(sec: &Section) -> Option<String> {
+    let SectionCategory::StandardSectionFull { containers } = &sec.category else {
+        return None;
+    };
+    let first = containers.first()?;
+    let Child::Block(thing) = first else {
+        return None;
+    };
+    let spans = thing
+        .iter()
+        .flat_map(|span| get_span_words(&span))
+        .collect::<String>();
+    Some(spans)
+}
+
+fn get_span_words(span: &Span) -> Vec<String> {
+    match span {
+        Span::Word { text, .. } => {
+            vec![text.to_string()]
+        }
+        Span::Space { .. } => vec![" ".to_string()],
+        Span::StandardSpan { spans, .. } => spans
+            .iter()
+            .map(|span| get_span_words(&span))
+            .collect::<Vec<Vec<String>>>()
+            .concat(),
+        _ => vec!["".to_string()],
+    }
+}
+
+fn get_title_section_title(ast: &Vec<Child>) -> Option<String> {
+    ast.iter().find_map(|child| match child {
+        Child::Section(sec) => {
+            if sec.r#type == String::from("title") {
+                filter_section(sec)
+            } else {
+                None
+            }
+        }
+        _ => None,
+    })
 }
