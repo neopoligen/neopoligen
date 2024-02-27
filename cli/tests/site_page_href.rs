87611d659c98aa399275a1d68d1bb5aecd76aa3d
Alan Smith <commit@alanwsmith.com>
2024-02-26 20:44:36 -0500

    Added in commented output tests that need to be replaced

---
+++ cli/tests/site_page_href.rs
@@ -0,0 +1,13 @@
+mod site_page_href {
+    // use minijinja::Value;
+    // use neopoligen::site::Site;
+    // use pretty_assertions::assert_eq;
+
+    // #[test]
+    // pub fn basic_href() {
+    //     let site = Site::site1();
+    //     let left = Some("/en/id_index/?integration-test-site".to_string());
+    //     let right = site.page_href(&[Value::from("id_index")]);
+    //     assert_eq!(left, right);
+    // }
+}
---
+++ cli/tests/site_page_href_title.rs
@@ -0,0 +1,20 @@
+mod page_href_title {
+    // use neopoligen::site::Site;
+    // use pretty_assertions::assert_eq;
+
+    // #[test]
+    // pub fn basic_href_title() {
+    //     let site = Site::site2();
+    //     let left = Some("site-2-home-page".to_string());
+    //     let right = site.page_href_title("id_index");
+    //     assert_eq!(left, right);
+    // }
+
+    // #[test]
+    // pub fn ensure_url_encoding() {
+    //     let site = Site::site1();
+    //     let left = Some("this-is-page%2Ffile-alfa".to_string());
+    //     let right = site.page_href_title("page-alfa");
+    //     assert_eq!(left, right);
+    // }
+}
