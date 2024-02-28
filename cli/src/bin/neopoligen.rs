4f3bf90f5d35ae47e4cae141bebad312e8bd2347
Alan Smith <commit@alanwsmith.com>
2024-02-28 00:13:41 -0500

    working on template testing output

--- cli/src/bin/neopoligen.rs
+++ cli/src/bin/neopoligen.rs
@@ -2,6 +2,15 @@ use dirs::document_dir;
 use neopoligen::builder::Builder;
 use neopoligen::config::Config;
 use neopoligen::file_set::FileSet;
+use neopoligen::helpers::get_file_paths_for_extension::get_file_paths_for_extension;
+use nom::branch::alt;
+use nom::bytes::complete::is_not;
+use nom::bytes::complete::tag;
+use nom::bytes::complete::take_until;
+use nom::character::complete::multispace0;
+use nom::combinator::rest;
+use nom::multi::many1;
+use nom::IResult;
 use serde::Deserialize;
 use std::fs;
 
@@ -26,11 +35,14 @@ fn main() {
                 site_root.push("Neopoligen");
                 site_root.push(engine_config.settings.active_site);
                 let config = Config::new(site_root);
+
+                // Testing (to move to a better location later)
+                test_templates(&config);
+
                 let mut file_set = FileSet::new();
                 file_set.load_content(&config.folders.content_root);
                 file_set.load_templates(&config.folders.theme_root);
                 let builder = Builder::new(file_set, &config);
-                builder.test_templates();
                 builder.write_files();
             }
             Err(e) => {
@@ -42,4 +54,98 @@ fn main() {
             println!("{}", e)
         }
     }
+}
+
+fn test_templates(config: &Config) {
+    let mut file_set = FileSet::new();
+    file_set.load_templates(&config.folders.theme_root);
+    get_file_paths_for_extension(&config.folders.theme_tests_root, "txt")
+        .iter()
+        .for_each(|tf| {
+            let test_setup = fs::read_to_string(tf).unwrap();
+            match parse_test_file(&test_setup) {
+                Ok(parts) => {
+                    dbg!(parts);
+                    ()
+                }
+                Err(e) => println!("{}", e),
+            }
+        });
+}
+
+#[derive(Debug)]
+enum TestSection {
+    Description(String),
+    Input(String, String),
+    Output(String),
+    SupportPage(String),
+    Template(String, String),
+}
+
+fn parse_test_file(source: &str) -> IResult<&str, Vec<TestSection>> {
+    let (source, sections) = many1(test_section)(source)?;
+    Ok((source, sections))
+}
+
+fn test_section(source: &str) -> IResult<&str, TestSection> {
+    let (source, string) = alt((
+        test_desc,
+        test_template,
+        test_support_page,
+        test_input,
+        test_output,
+    ))(source)?;
+    Ok((source, string))
+}
+
+fn test_desc(source: &str) -> IResult<&str, TestSection> {
+    let (source, _) = multispace0(source)?;
+    let (source, _) = tag("### DESCRIPTION ###")(source)?;
+    let (source, _) = multispace0(source)?;
+    let (source, desc) = take_until("###")(source)?;
+    Ok((source, TestSection::Description(desc.trim().to_string())))
+}
+
+fn test_template(source: &str) -> IResult<&str, TestSection> {
+    let (source, _) = multispace0(source)?;
+    let (source, _) = tag("### TEMPLATE ###")(source)?;
+    let (source, _) = multispace0(source)?;
+    let (source, name) = take_until("~~~")(source)?;
+    let (source, _) = tag("~~~")(source)?;
+    let (source, _) = multispace0(source)?;
+    let (source, template) = take_until("###")(source)?;
+    Ok((
+        source,
+        TestSection::Template(name.trim().to_string(), template.trim().to_string()),
+    ))
+}
+
+fn test_support_page(source: &str) -> IResult<&str, TestSection> {
+    let (source, _) = multispace0(source)?;
+    let (source, _) = tag("### SUPPORT_PAGE ###")(source)?;
+    let (source, _) = multispace0(source)?;
+    let (source, desc) = take_until("###")(source)?;
+    Ok((source, TestSection::SupportPage(desc.trim().to_string())))
+}
+
+fn test_input(source: &str) -> IResult<&str, TestSection> {
+    let (source, _) = multispace0(source)?;
+    let (source, _) = tag("### INPUT ###")(source)?;
+    let (source, _) = multispace0(source)?;
+    let (source, content) = take_until("###")(source)?;
+    let (id_source, _) = take_until("-- id: ")(content)?;
+    let (id_source, _) = tag("-- id: ")(id_source)?;
+    let (_, id) = is_not(" \n")(id_source)?;
+    Ok((
+        source,
+        TestSection::Input(id.trim().to_string(), content.trim().to_string()),
+    ))
+}
+
+fn test_output(source: &str) -> IResult<&str, TestSection> {
+    let (source, _) = multispace0(source)?;
+    let (source, _) = tag("### OUTPUT ###")(source)?;
+    let (source, _) = multispace0(source)?;
+    let (source, content) = rest(source)?;
+    Ok((source, TestSection::SupportPage(content.trim().to_string())))
 }
--- cli/src/builder/mod.rs
+++ cli/src/builder/mod.rs
@@ -2,19 +2,10 @@ pub mod new;
 
 use crate::config::Config;
 use crate::file_set::FileSet;
-use crate::helpers::get_file_paths_for_extension::get_file_paths_for_extension;
 use crate::site::Site;
 use minijinja::context;
 use minijinja::Environment;
 use minijinja::Value;
-use nom::branch::alt;
-use nom::bytes::complete::is_not;
-use nom::bytes::complete::tag;
-use nom::bytes::complete::take_until;
-use nom::character::complete::multispace0;
-use nom::combinator::rest;
-use nom::multi::many1;
-use nom::IResult;
 use std::collections::BTreeMap;
 use std::fs;
 use std::fs::create_dir_all;
@@ -81,21 +72,6 @@ impl Builder {
         outputs
     }
 
-    pub fn test_templates(&self) {
-        get_file_paths_for_extension(&self.config.folders.theme_tests_root, "txt")
-            .iter()
-            .for_each(|tf| {
-                let test_setup = fs::read_to_string(tf).unwrap();
-                match parse_test_file(&test_setup) {
-                    Ok(parts) => {
-                        dbg!(parts);
-                        ()
-                    }
-                    Err(e) => println!("{}", e),
-                }
-            });
-    }
-
     pub fn write_files(&self) {
         self.files_to_output().iter().for_each(|f| {
             let output_path = PathBuf::from(f.0);
@@ -104,81 +80,4 @@ impl Builder {
             let _ = fs::write(output_path, f.1);
         });
     }
-}
-
-#[derive(Debug)]
-enum TestSection {
-    Description(String),
-    Input(String, String),
-    Output(String),
-    SupportPage(String),
-    Template(String, String),
-}
-
-fn parse_test_file(source: &str) -> IResult<&str, Vec<TestSection>> {
-    let (source, sections) = many1(test_section)(source)?;
-    Ok((source, sections))
-}
-
-fn test_section(source: &str) -> IResult<&str, TestSection> {
-    let (source, string) = alt((
-        test_desc,
-        test_template,
-        test_support_page,
-        test_input,
-        test_output,
-    ))(source)?;
-    Ok((source, string))
-}
-
-fn test_desc(source: &str) -> IResult<&str, TestSection> {
-    let (source, _) = multispace0(source)?;
-    let (source, _) = tag("### DESCRIPTION ###")(source)?;
-    let (source, _) = multispace0(source)?;
-    let (source, desc) = take_until("###")(source)?;
-    Ok((source, TestSection::Description(desc.trim().to_string())))
-}
-
-fn test_template(source: &str) -> IResult<&str, TestSection> {
-    let (source, _) = multispace0(source)?;
-    let (source, _) = tag("### TEMPLATE ###")(source)?;
-    let (source, _) = multispace0(source)?;
-    let (source, name) = take_until("~~~")(source)?;
-    let (source, _) = tag("~~~")(source)?;
-    let (source, _) = multispace0(source)?;
-    let (source, template) = take_until("###")(source)?;
-    Ok((
-        source,
-        TestSection::Template(name.trim().to_string(), template.trim().to_string()),
-    ))
-}
-
-fn test_support_page(source: &str) -> IResult<&str, TestSection> {
-    let (source, _) = multispace0(source)?;
-    let (source, _) = tag("### SUPPORT_PAGE ###")(source)?;
-    let (source, _) = multispace0(source)?;
-    let (source, desc) = take_until("###")(source)?;
-    Ok((source, TestSection::SupportPage(desc.trim().to_string())))
-}
-
-fn test_input(source: &str) -> IResult<&str, TestSection> {
-    let (source, _) = multispace0(source)?;
-    let (source, _) = tag("### INPUT ###")(source)?;
-    let (source, _) = multispace0(source)?;
-    let (source, content) = take_until("###")(source)?;
-    let (id_source, _) = take_until("-- id: ")(content)?;
-    let (id_source, _) = tag("-- id: ")(id_source)?;
-    let (_, id) = is_not(" \n")(id_source)?;
-    Ok((
-        source,
-        TestSection::Input(id.trim().to_string(), content.trim().to_string()),
-    ))
-}
-
-fn test_output(source: &str) -> IResult<&str, TestSection> {
-    let (source, _) = multispace0(source)?;
-    let (source, _) = tag("### OUTPUT ###")(source)?;
-    let (source, _) = multispace0(source)?;
-    let (source, content) = rest(source)?;
-    Ok((source, TestSection::SupportPage(content.trim().to_string())))
 }
