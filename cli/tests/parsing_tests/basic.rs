58a2e87a37de33f28f2a0e2f249527f496d4ac74
Alan Smith <commit@alanwsmith.com>
2024-02-28 17:26:11 -0500

    cleanup

--- cli/tests/parsing_tests/basic.rs
+++ cli/tests/parsing_tests/basic.rs
@@ -5,33 +5,16 @@
 // for the function that has the issue
 
 mod parsing_basic_tests {
-    // use minijinja::Value;
-    // use neopoligen::child::Child;
     use neopoligen::config::Config;
     use neopoligen::file_set::FileSet;
     use neopoligen::builder::Builder;
-    // use neopoligen::section::Section;
-    // use neopoligen::section_category::SectionCategory;
     use neopoligen::site::Site;
-    // use neopoligen::span::Span;
-    // use pretty_assertions::assert_eq;
-    // use std::collections::BTreeMap;
-    // use std::collections::BTreeSet;
 
     #[test]
     pub fn basic() {
         let file_set = FileSet::parsing_tests();
         let config = Config::parsing_tests();
-        let site = Site::new(&file_set, &config);
-        let builder = Builder::new(file_set, &config);
-
-        
-        // let ast = &site.pages.get("home-page").unwrap().ast;
-        // dbg!(ast);
-
-        // let left = Value::from_serializable::<Vec<Child>>(&response);
-        // let right = site.pages.get("home-page");
-        // assert_eq!(left, right);
-
+        let _site = Site::new(&file_set, &config);
+        let _builder = Builder::new(file_set, &config);
     }
 }
