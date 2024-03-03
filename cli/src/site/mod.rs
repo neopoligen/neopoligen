10c9794a4a6a8e2db2fc03d9b82f2fc78500b106
Alan Smith <commit@alanwsmith.com>
2024-03-03 18:04:44 -0500

    moving nav items to their own folder

--- cli/src/site/mod.rs
+++ cli/src/site/mod.rs
@@ -1,3 +1,4 @@
+pub mod nav_items;
 pub mod new;
 pub mod object;
 
@@ -5,7 +6,7 @@ use crate::child::Child;
 use crate::config::Config;
 use crate::nav_item::NavItem;
 use crate::nav_item::NavItemType;
-use crate::nav_tree::NavPrevNextItem;
+// use crate::nav_tree::NavPrevNextItem;
 use crate::nav_tree::NavTree;
 use crate::page::Page;
 use crate::section::Section;
@@ -72,152 +73,6 @@ impl Site {
     pub fn log_from_template(&self, args: &[Value]) -> String {
         event!(Level::INFO, "{}", args[0].to_string());
         "".to_string()
-    }
-
-    pub fn folder_menu(&self, args: &[Value]) -> Vec<NavItem> {
-        let mut items = self.folder_menu_builder(args);
-        items
-            .iter_mut()
-            .for_each(|item| self.folder_menu_set_open_closed_folders(args, item));
-        items
-    }
-
-    pub fn folder_menu_set_open_closed_folders(&self, args: &[Value], item: &mut NavItem) {
-        if matches!(item.item_type, NavItemType::ClosedFolderTitle) {
-            let page_folders = self.page_folders(args);
-            if page_folders
-                .into_iter()
-                .take(item.folders.len())
-                .collect::<Vec<String>>()
-                == item.folders
-            {
-                item.item_type = NavItemType::OpenedFolderTitle;
-            } else {
-                item.item_type = NavItemType::ClosedFolderTitle;
-            }
-        }
-
-        if matches!(item.item_type, NavItemType::ClosedFolderIndex) {
-            let page_folders = self.page_folders(args);
-            if page_folders
-                .into_iter()
-                .take(item.folders.len())
-                .collect::<Vec<String>>()
-                == item.folders
-            {
-                item.item_type = NavItemType::OpenedFolderIndex;
-            } else {
-                item.item_type = NavItemType::ClosedFolderIndex;
-            }
-        }
-
-        item.children
-            .iter_mut()
-            .for_each(|i| self.folder_menu_set_open_closed_folders(args, i))
-
-        // item.folders.iter().enumerate().for_each(|(index, folder)| {
-        //     dbg!("asdf");
-        //     ()
-        // });
-
-        // if item
-        //     .folders
-        //     .iter()
-        //     .all(|folder| page_folders.contains(folder))
-        // {
-        //     item.item_type = NavItemType::OpenFolderTitle;
-        // } else {
-        //     dbg!(&page_folders);
-        //     dbg!(&item.folders);
-        //     item.item_type = NavItemType::ClosedFolderTitle;
-        // }
-    }
-
-    pub fn folder_menu_sort_by_path(&self, items: &mut Vec<NavItem>) {
-        items.sort_by_key(|k| k.path_sort_string.clone());
-        items
-            .iter_mut()
-            .for_each(|item| self.folder_menu_sort_by_path(&mut item.children));
-    }
-
-    #[instrument(skip(self))]
-    pub fn folder_menu_builder(&self, args: &[Value]) -> Vec<NavItem> {
-        let menu_key = args[1]
-            .try_iter()
-            .unwrap()
-            .into_iter()
-            .map(|f| f.try_iter().unwrap().into_iter().join("-"))
-            .join("-");
-        match self.get_cache("menus", &menu_key) {
-            Some(menu_cache) => {
-                if let CacheObject::Menu(menu) = menu_cache {
-                    menu
-                } else {
-                    vec![]
-                }
-            }
-            None => {
-                let mut r: Vec<NavItem> = args[1]
-                    .try_iter()
-                    .unwrap()
-                    .filter_map(|folder_vecs| {
-                        let folder_pattern: Vec<String> = folder_vecs
-                            .try_iter()
-                            .unwrap()
-                            .map(|f| f.to_string())
-                            .collect();
-                        self.folder_menu_index_finder(folder_pattern)
-                    })
-                    .collect();
-                r.iter_mut()
-                    .for_each(|item| self.folder_menu_sort_by_path(&mut item.children));
-                self.set_cache("menus", menu_key, CacheObject::Menu(r.clone()));
-                r
-            }
-        }
-
-        // event!(Level::INFO, "fn folder_menu_builder");
-        // let mut binding = self.cache.lock().unwrap();
-        // let menus = binding.get("menus").unwrap();
-        // match menus.get(&menu_key) {
-        //     Some(menu_cache) => {
-        //         if let CacheObject::Menu(menu) = menu_cache {
-        //             menu.to_vec()
-        //         } else {
-        //             vec![]
-        //         }
-        //     }
-        //     None => {
-        //         let r = args[1]
-        //             .try_iter()
-        //             .unwrap()
-        //             .filter_map(|folder_vecs| {
-        //                 let folder_pattern: Vec<String> = folder_vecs
-        //                     .try_iter()
-        //                     .unwrap()
-        //                     .map(|f| f.to_string())
-        //                     .collect();
-        //                 self.folder_menu_index_finder(folder_pattern)
-        //             })
-        //             .collect();
-        //         r
-        //     }
-        // }
-
-        // let r = args[1]
-        //     .try_iter()
-        //     .unwrap()
-        //     .filter_map(|folder_vecs| {
-        //         let folder_pattern: Vec<String> = folder_vecs
-        //             .try_iter()
-        //             .unwrap()
-        //             .map(|f| f.to_string())
-        //             .collect();
-        //         self.folder_menu_index_finder(folder_pattern)
-        //     })
-        //     .collect();
-        // r
-        // }
     }
 
     #[instrument(skip(self))]
@@ -299,15 +154,6 @@ impl Site {
                 }
             })
         }
-    }
-
-    // TODO: Set this up so it pulls actual link text
-    pub fn nav_link_title_link(&self, args: &[Value]) -> Option<String> {
-        Some(format!(
-            r#"<a href="{}">{}</a>"#,
-            self.page_href(args).unwrap(),
-            self.page_title(args).unwrap()
-        ))
     }
 
     #[instrument(skip(self))]
@@ -396,37 +242,6 @@ impl Site {
                 None => None,
             }
         }
-    }
-
-    pub fn nav_from_files_and_folders(&self, args: &[Value]) -> NavTree {
-        let mut nav_links = NavTree {
-            items: self.folder_menu(args),
-            prev_item: None,
-            next_item: None,
-            prev_next_order: vec![],
-        };
-        self.set_current_file_for_nav_links(&args[0].to_string(), &mut nav_links);
-        nav_links
-    }
-
-    pub fn nav_from_files_and_folders_dev(&self, args: &[Value]) -> NavTree {
-        let mut nav_links = NavTree {
-            items: self.folder_menu(args),
-            prev_item: None,
-            next_item: None,
-            prev_next_order: vec![],
-        };
-        self.set_current_file_for_nav_links(&args[0].to_string(), &mut nav_links);
-        self.find_prev_next_nav_links(&args[0].to_string(), &mut nav_links);
-        nav_links
-    }
-
-    pub fn find_prev_next_nav_links(&self, _id: &String, links: &mut NavTree) {
-        links.next_item = Some(NavPrevNextItem {
-            page_id: "content-bravo".to_string(),
-            title_link_or_text: None,
-        });
-        dbg!("here");
     }
 
     pub fn page_folders(&self, args: &[Value]) -> Vec<String> {
