1e35e20832b04ff4d1c83206620f774a7e011ad3
Alan Smith <commit@alanwsmith.com>
2024-03-04 11:29:03 -0500

    cleanup

--- cli/src/page/mod.rs
+++ cli/src/page/mod.rs
@@ -1,20 +1,9 @@
-// pub mod mocks;
 pub mod new;
 pub mod parse;
 
 use crate::child::Child;
 use serde::Serialize;
 use std::path::PathBuf;
-
-pub enum ParsedPage {
-    ValidPage(Page),
-    InvalidPage {
-        path: PathBuf,
-        source: String,
-        remainder: Option<String>,
-        error: Option<String>,
-    },
-}
 
 #[derive(Clone, Debug, PartialEq, Serialize)]
 #[serde(tag = "type", rename_all = "lowercase")]
@@ -26,10 +15,4 @@ pub struct Page {
     pub publish: bool,
     // these items are generated on creation
     pub title: Option<String>,
-}
-
-#[cfg(test)]
-mod page_functions {
-    // use super::*;
-    // use pretty_assertions::assert_eq;
 }
