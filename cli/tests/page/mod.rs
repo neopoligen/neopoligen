599cac046f09c354214a178c7c0edf39ef529084
Alan Smith <commit@alanwsmith.com>
2024-03-04 11:46:38 -0500

    pointed to set1

--- cli/tests/page/mod.rs
+++ cli/tests/page/mod.rs
@@ -6,8 +6,8 @@ use pretty_assertions::assert_eq;
 #[test]
 #[ignore]
 fn title_from_title_section() {
-    let file_set = FileSet::nav_items2();
-    let config = Config::nav_items2();
+    let file_set = FileSet::set1();
+    let config = Config::set1();
     let site = Site::new(&file_set, &config);
     let left = Some("Home Page".to_string());
     let right = site.pages.get("aabb0011").unwrap().title.clone();
