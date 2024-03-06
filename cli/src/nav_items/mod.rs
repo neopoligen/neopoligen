25fcd035c62a67bc35018fd08eea83d27574aa4c
Alan Smith <commit@alanwsmith.com>
2024-03-05 21:29:56 -0500

    Cleanup

--- cli/src/nav_items/mod.rs
+++ cli/src/nav_items/mod.rs
@@ -4,35 +4,6 @@
 // to collections. once everything's there this
 // can be delted
 
-// #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
-// #[serde(tag = "type", rename_all = "lowercase")]
-// pub struct NavItems {
-//     pub tree: Vec<NavItem>,
-//     pub prev_next_items: Vec<NavItem>,
-//     pub next_item: Option<NavItem>,
-//     pub prev_item: Option<NavItem>,
-//     pub open_folders: Vec<String>,
-//     pub current_item: Option<NavItem>,
-// }
-
-// impl NavItems {
-//     pub fn tree_items_from(&self, args: &[Value]) -> Vec<NavId> {
-//         let page_id = args[0].to_string();
-//         match self
-//             .tree
-//             .iter()
-//             .find_map(|item| is_tree_sub_root(item, &page_id))
-//         {
-//             Some(sub_root) => sub_root
-//                 .children
-//                 .iter()
-//                 .map(|child| get_nav_id(child))
-//                 .collect(),
-//             None => vec![],
-//         }
-//     }
-// }
-
 // fn get_prev_item(key: &String, items: &Vec<NavItem>) -> Option<NavItem> {
 //     match items.iter().position(|test_item| &test_item.page_id == key) {
 //         Some(index) => {
--- cli/src/nav_items/new_from_files_and_folders.rs
+++ cli/src/nav_items/new_from_files_and_folders.rs
@@ -2,13 +2,6 @@
 // Delete this when all the features are implemented
 // in collections
 
-// fn do_sort_by_source_path(items: &mut Vec<NavItem>) {
-//     items.sort_by_key(|k| k.path_sort_string.clone());
-//     items
-//         .iter_mut()
-//         .for_each(|item| do_sort_by_source_path(&mut item.children));
-// }
-
 // fn load_prev_next(tree: &Vec<NavItem>) -> Vec<NavItem> {
 //     let mut prev_next_vec: Vec<NavItem> = vec![];
 //     prev_next_flattener(tree, &mut prev_next_vec);
@@ -29,50 +22,3 @@
 //         prev_next_flattener(&item.children, dest);
 //     });
 // }
-
-//     fn folder_menu_set_open_closed_folders(&self, args: &[Value], item: &mut NavItem) {
-//         if matches!(item.item_type, NavItemType::TitleFolderClosed) {
-//             let page_folders = self.page_folders(args);
-//             if page_folders
-//                 .into_iter()
-//                 .take(item.folders.len())
-//                 .collect::<Vec<String>>()
-//                 == item.folders
-//             {
-//                 item.item_type = NavItemType::TitleFolderOpened;
-//             } else {
-//                 item.item_type = NavItemType::TitleFolderClosed;
-//             }
-//         }
-//         if matches!(item.item_type, NavItemType::IndexFolderClosed) {
-//             let page_folders = self.page_folders(args);
-//             if page_folders
-//                 .into_iter()
-//                 .take(item.folders.len())
-//                 .collect::<Vec<String>>()
-//                 == item.folders
-//             {
-//                 item.item_type = NavItemType::IndexFolderOpened;
-//             } else {
-//                 item.item_type = NavItemType::IndexFolderClosed;
-//             }
-//         }
-//         item.children
-//             .iter_mut()
-//             .for_each(|i| self.folder_menu_set_open_closed_folders(args, i))
-//         // item.folders.iter().enumerate().for_each(|(index, folder)| {
-//         //     dbg!("asdf");
-//         //     ()
-//         // });
-//         // if item
-//         //     .folders
-//         //     .iter()
-//         //     .all(|folder| page_folders.contains(folder))
-//         // {
-//         //     item.item_type = NavItemType::OpenFolderTitle;
-//         // } else {
-//         //     dbg!(&page_folders);
-//         //     dbg!(&item.folders);
-//         //     item.item_type = NavItemType::TitleFolderClosed;
-//         // }
-//     }
