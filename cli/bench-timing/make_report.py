5b0c1270bfccc1b528a34db4131b81d3d4f80993
Alan Smith <commit@alanwsmith.com>
2024-04-17 10:13:22 -0400

    progress

--- cli/bench-timing/make_report.py
+++ cli/bench-timing/make_report.py
@@ -4,13 +4,14 @@ import re
 
 print("running")
 
-pattern = re.compile('(INFO)\s[^:]+:(.*?)\{.*?\|\|(.*)\|\|')
+pattern = re.compile('(INFO)\s[^:]+:(.*?)\{.*?\|\|([\d.]+)(.*)\|\|')
 
 with open("/Users/alan/Documents/Neopoligen/log.log") as _in:
     lines = _in.readlines()
     for line in lines:
         matches = re.search(pattern, line)
         if matches:
-            print(matches.group(2))
-            print(matches.group(3))
+            print(f"{matches.group(2)} - {matches.group(3)} - {matches.group(4)}")
+
+
 
