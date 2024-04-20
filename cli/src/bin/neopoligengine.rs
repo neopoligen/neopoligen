d268ea0ae637b948ff425bc3a0e7433b6e2152da
Alan Smith <commit@alanwsmith.com>
2024-04-20 06:43:47 -0400

    Staring work on new template approach

--- cli/src/bin/neopoligengine.rs
+++ cli/src/bin/neopoligengine.rs
@@ -5,7 +5,7 @@ use neopoligengine::config::Config;
 use neopoligengine::file_set::FileSet;
 use neopoligengine::neo_config::NeoConfig;
 use neopoligengine::neo_config::NeoEnv;
-use neopoligengine::template_tester::*;
+use neopoligengine::template_tester::test_templates;
 use notify_debouncer_mini::new_debouncer;
 use notify_debouncer_mini::notify::RecursiveMode;
 use notify_debouncer_mini::DebounceEventResult;
