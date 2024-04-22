411f09f86429f73460fefd997ec4005c6173f24b
Alan Smith <commit@alanwsmith.com>
2024-04-22 13:53:23 -0400

    cleanup

--- cli/src/builder/mod.rs
+++ cli/src/builder/mod.rs
@@ -71,103 +71,6 @@ impl Builder {
         }
     }
 
-    // // Deprecated in favor of generate_files method
-    // pub fn files_to_output(&self) -> BTreeMap<PathBuf, String> {
-    //     let mut env = Environment::new();
-    //     env.set_syntax(Syntax {
-    //         block_start: "[!".into(),
-    //         block_end: "!]".into(),
-    //         variable_start: "[@".into(),
-    //         variable_end: "@]".into(),
-    //         comment_start: "[#".into(),
-    //         comment_end: "#]".into(),
-    //     })
-    //     .unwrap();
-    //     let site = Site::new(&self.file_set, &self.config);
-    //     let mut outputs = BTreeMap::new();
-    //     self.file_set
-    //         .templates
-    //         .iter()
-    //         .for_each(|t| env.add_template_owned(t.0, t.1).unwrap());
-    //     env.add_template_owned(
-    //         "splitter.jinja".to_string(),
-    //         r#"[! import "includes/macros.jinja" as macros !]
-    // [# include "global_vars" #]
-    // [! for page_id in site.page_ids() !]
-    // [@ site.log({ "page_id": page_id, "source_path": site.page_source_path(page_id) }) @]
-    // [@ site.page_build_path(page_id) @]
-    // --- PAGE_DATA_SPLIT ---
-    // [! include site.page_template(page_id) !]
-    // --- PAGE_SEPARATOR ---
-    // [! endfor !]"#
-    //             .to_string(),
-    //     )
-    //     .unwrap();
-    //     match env.get_template("splitter.jinja") {
-    //         Ok(splitter) => {
-    //             match splitter.render(context!(
-    //                  site => Value::from_object(site),
-    //             )) {
-    //                 Ok(combined_pages) => {
-    //                     combined_pages
-    //                         .split("--- PAGE_SEPARATOR ---")
-    //                         .for_each(|page| {
-    //                             let page_parts: Vec<_> =
-    //                                 page.split("--- PAGE_DATA_SPLIT ---").collect();
-    //                             if page_parts.len() == 2 {
-    //                                 outputs.insert(
-    //                                     PathBuf::from(page_parts[0].trim()),
-    //                                     page_parts[1].trim().to_string(),
-    //                                 );
-    //                             }
-    //                         });
-    //                 }
-    //                 Err(e) => {
-    //                     println!("SPLITTER ERROR 1 - {}", e);
-    //                     // NOTE: Below is an attempt to find errors, but it's slow
-    //                     // and look like logging back in from the templates might
-    //                     // work just fine
-    //                     // {
-    //                     //     let mut redo_env = Environment::new();
-    //                     //     self.file_set
-    //                     //         .templates
-    //                     //         .iter()
-    //                     //         .for_each(|t| redo_env.add_template_owned(t.0, t.1).unwrap());
-    //                     //     let site_redo_wrapper = Site::new(&self.file_set, &self.config);
-    //                     //     site_redo_wrapper.pages.iter().for_each(|redo_page| {
-    //                     //         let redo_site = Site::new(&self.file_set, &self.config);
-    //                     //         let redo_template = redo_env
-    //                     //             .get_template(
-    //                     //                 &redo_site
-    //                     //                     .page_template(&[Value::from(redo_page.0.to_string())])
-    //                     //                     .unwrap(),
-    //                     //             )
-    //                     //             .unwrap();
-    //                     //         match redo_template.render(context!(
-    //                     //             site => Value::from_object(redo_site),
-    //                     //             page_id => Value::from("2ck5rhdc")
-    //                     //         )) {
-    //                     //             // Ok(out) => println!("{}", out),
-    //                     //             Ok(_) => (),
-    //                     //             Err(e) => println!(
-    //                     //                 "PAGE: {}\nERROR: {}\n",
-    //                     //                 redo_page.1.source_path.display(),
-    //                     //                 e
-    //                     //             ),
-    //                     //         };
-    //                     //         // dbg!(redo_page.0);
-    //                     //     });
-    //                     // }
-    //                 }
-    //             }
-    //         }
-    //         Err(e) => {
-    //             println!("SPLITTER ERROR 2 - {}", e);
-    //         }
-    //     };
-    //     outputs
-    // }
-
     #[instrument(skip(self))]
     pub fn generate_files(&mut self) {
         let mut env = Environment::new();
@@ -248,14 +151,6 @@ impl Builder {
         );
     }
 
-    // #[instrument(skip(self))]
-    // pub fn get_changed_files(&self) {
-    //     let now = Instant::now();
-    //     // TODO: Implement page cache stuff here
-    //     event!(Level::DEBUG, "||{:?}||", now.elapsed());
-    // }
-    //
-
     #[instrument(skip(self))]
     pub fn output_files(&self) {
         self.outputs.iter().for_each(|output| {
@@ -273,48 +168,6 @@ impl Builder {
             event!(Level::INFO, "Writing: {}", output.0.display());
         });
     }
-
-    // Deprecated in favor of generate_files method
-    #[instrument(skip(self))]
-    pub fn write_changed_files(&self) {
-        let mut page_hash_cache_path = config_local_dir().unwrap();
-        page_hash_cache_path.push("Neopoligen");
-        page_hash_cache_path.push("page-hash-caches");
-        page_hash_cache_path.push(format!(
-            "{}.json",
-            &self.neo_env.active_site.clone().unwrap()
-        ));
-        if !file_exists(&page_hash_cache_path) {
-            event!(
-                Level::DEBUG,
-                "Making new page hash cache at: {}",
-                page_hash_cache_path.display()
-            );
-        }
-    }
-
-    // // Deprecated in favor of generate_files method
-    // #[instrument(skip(self))]
-    // pub fn write_files(&self) {
-    //     event!(Level::DEBUG, "fn write_files");
-    //     println!("Writing files");
-    //     // dbg!(&self.config);
-    //     self.files_to_output().iter().for_each(|f| {
-    //         // println!("{}", f.0.clone().display());
-    //         if f.0
-    //             .starts_with(self.config.folders.build_root.display().to_string())
-    //         {
-    //             let build_path = PathBuf::from(f.0);
-    //             // dbg!(&output_path);
-    //             // println!("{}", &f.0.display());
-    //             let parent_dir = build_path.parent().unwrap();
-    //             let _ = create_dir_all(parent_dir);
-    //             let _ = fs::write(build_path, f.1);
-    //         } else {
-    //             println!("ERROR: Tried to write outside of the output root");
-    //         }
-    //     });
-    // }
 }
 
 // TODO: if you ever have to mess with this, change it to use
