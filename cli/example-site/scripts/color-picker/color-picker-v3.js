257fb2270e0f666a3fd4e979f85a5e0150b6da9a
Alan Smith <commit@alanwsmith.com>
2024-04-30 12:45:22 -0400

    working on lining colors up again

--- cli/example-site/scripts/color-picker/color-picker-v3.js
+++ cli/example-site/scripts/color-picker/color-picker-v3.js
@@ -198,7 +198,12 @@ customElements.define(
             </div>
         </div>
         <div class="section-wrapper">
-            <h4>Secondary Colors</h4>
+        <h3>Secondary Colors: 
+            <span class="dev-color-charlie">charlie</span>
+            <span class="dev-color-delta">delta</span>
+            <span class="dev-color-echo">echo</span>
+            <span class="dev-color-foxtrot">foxtrot</span>
+         </h3>
             <div class="secondary-section">
                 <div class="secondary-wrapper charliedelta-section">
                     <div class="charliedelta-chips"></div>
@@ -1035,25 +1040,54 @@ h2, h3 {
 
     getFoxtrot(mode) {
       let payload = []
+
+      //   payload.push(
+      //     (this.state.modes[mode].l +
+      //       this.state.modes[mode].colors.bravo.l +
+      //       this.state.base.l.max +
+      //       this.state.base.l.interval *
+      //         this.state.collections[
+      //           this.state.modes[mode].colors.bravo.collectionIndex
+      //         ][1][0]) %
+      //       this.state.base.l.max
+      //   )
+      //   payload.push(
+      //     (this.state.modes[mode].c +
+      //       this.state.modes[mode].colors.bravo.c +
+      //       this.state.base.c.max +
+      //       this.state.base.c.interval *
+      //         this.state.collections[
+      //           this.state.modes[mode].colors.bravo.collectionIndex
+      //         ][1][1]) %
+      //       this.state.base.c.max
+      //   )
+      //   payload.push(
+      //     (this.state.modes[mode].h +
+      //       this.state.modes[mode].colors.bravo.h +
+      //       this.state.modes[mode].colors.bravo.collectionShift) %
+      //       360
+      //   )
+
       payload.push(
         (this.state.modes[mode].l +
           this.state.modes[mode].colors.bravo.l +
           this.state.base.l.max +
-          this.state.base.l.interval *
-            this.state.collections[
-              this.state.modes[mode].colors.bravo.collectionIndex
-            ][1][0]) %
-          this.state.base.l.max
+          this.state.collections[
+            this.state.modes[mode].colors.bravo.collectionIndex
+          ][1][0] *
+            this.state.base.l.interval) %
+          100
       )
       payload.push(
-        (this.state.modes[mode].c +
+        ((this.state.modes[mode].c * 10 +
           this.state.modes[mode].colors.bravo.c +
-          this.state.base.c.max +
-          this.state.base.c.interval *
-            this.state.collections[
-              this.state.modes[mode].colors.bravo.collectionIndex
-            ][1][1]) %
-          this.state.base.c.max
+          this.state.base.c.max * 10 +
+          this.state.collections[
+            this.state.modes[mode].colors.bravo.collectionIndex
+          ][1][1] *
+            (this.state.base.c.interval * 10)) %
+          5) /
+          10
       )
       payload.push(
         (this.state.modes[mode].h +
@@ -1061,6 +1095,7 @@ h2, h3 {
           this.state.modes[mode].colors.bravo.collectionShift) %
           360
       )
+
       return payload
     }
 
@@ -1622,6 +1657,8 @@ h2, h3 {
     }
 
     sendStylesheet() {
+      // TODO: Add bw-match and bw-reverse
+
       let styles = `
 :root {
     --size-1: 2.986rem;
