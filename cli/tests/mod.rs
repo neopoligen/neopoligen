b720d25b6e6f99a046c331db077f8350f8d6836b
Alan Smith <commit@alanwsmith.com>
2024-05-03 17:49:40 -0400

    Added initial section test

--- cli/tests/mod.rs
+++ cli/tests/mod.rs
@@ -1,5 +1,6 @@
 pub mod block;
 pub mod page;
 pub mod parser;
+pub mod section;
 pub mod span;
 pub mod spans;
---
+++ cli/tests/section/mod.rs
@@ -0,0 +1,67 @@
+use neopoligengine::block::*;
+use neopoligengine::section::*;
+use neopoligengine::span::*;
+use pretty_assertions::assert_eq;
+
+#[test]
+fn basic_section() {
+    let source = "-- p\n\nyankee romeo";
+    let left = Section::Standard {
+        attrs: vec![],
+        content: vec![Block::Paragraph {
+            spans: vec![
+                Span::WordPart {
+                    text: "yankee".to_string(),
+                },
+                Span::Space {
+                    text: " ".to_string(),
+                },
+                Span::WordPart {
+                    text: "romeo".to_string(),
+                },
+            ],
+        }],
+        r#type: "p".to_string(),
+    };
+    let right = section(source).unwrap().1;
+    assert_eq!(left, right);
+}
+
+#[test]
+fn multiple_paragraphs() {
+    let source = "-- div\n\nsierra tango\n\nindia lima\n\n";
+    let left = Section::Standard {
+        attrs: vec![],
+        content: vec![
+            Block::Paragraph {
+                spans: vec![
+                    Span::WordPart {
+                        text: "sierra".to_string(),
+                    },
+                    Span::Space {
+                        text: " ".to_string(),
+                    },
+                    Span::WordPart {
+                        text: "tango".to_string(),
+                    },
+                ],
+            },
+            Block::Paragraph {
+                spans: vec![
+                    Span::WordPart {
+                        text: "india".to_string(),
+                    },
+                    Span::Space {
+                        text: " ".to_string(),
+                    },
+                    Span::WordPart {
+                        text: "lima".to_string(),
+                    },
+                ],
+            },
+        ],
+        r#type: "div".to_string(),
+    };
+    let right = section(source).unwrap().1;
+    assert_eq!(left, right);
+}
