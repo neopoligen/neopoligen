d4fd044081973ccb7ff435f96a77521df786c85a
Alan Smith <commit@alanwsmith.com>
2024-05-05 09:14:01 -0400

    refactor to move basic to its own file

---
+++ cli/src/section/basic.rs
@@ -0,0 +1,70 @@
+use crate::block::*;
+// use crate::section::list::list_section;
+use crate::section::*;
+use crate::section_attr::*;
+use crate::span::empty_line;
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+// use nom::bytes::complete::take_until;
+use nom::character::complete::line_ending;
+use nom::character::complete::multispace0;
+use nom::character::complete::space0;
+use nom::character::complete::space1;
+use nom::combinator::eof;
+use nom::combinator::not;
+// use nom::combinator::rest;
+use nom::multi::many0;
+// use std::collections::BTreeMap;
+// use nom::multi::many1;
+use nom::sequence::tuple;
+use nom::IResult;
+use nom::Parser;
+use nom_supreme::error::ErrorTree;
+use nom_supreme::parser_ext::ParserExt;
+// use serde::Serialize;
+// use serde_json::Value;
+
+pub fn basic_section<'a>(
+    source: &'a str,
+    list: &'a Vec<String>,
+) -> IResult<&'a str, Section, ErrorTree<&'a str>> {
+    let (source, result) = list.iter().fold(initial_error(), |acc, item| match acc {
+        Ok(v) => Ok(v),
+        _ => basic_section_finder(source, item),
+    })?;
+    Ok((source, result))
+}
+
+fn basic_section_finder<'a>(
+    source: &'a str,
+    key: &'a str,
+) -> IResult<&'a str, Section, ErrorTree<&'a str>> {
+    let initial_source = source;
+    let (source, _) = tag("--").context("basic_section_finder").parse(source)?;
+    let (source, _) = space1.context("basic_section_finder").parse(source)?;
+    let (source, r#type) = tag(key).context("basic_section_finder").parse(source)?;
+    let (source, _) = not(tag("/"))
+        .context("basic_section_finder")
+        .parse(source)?;
+    let (source, _) = alt((tuple((multispace0, eof)), tuple((space0, line_ending))))
+        .context("basic_section_finder")
+        .parse(source)?;
+    let (source, attrs) = many0(section_attr)
+        .context("basic_section_finder")
+        .parse(source)?;
+    let (source, _) = alt((empty_line.map(|_| ""), eof))
+        .context("basic_section_finder")
+        .parse(source)?;
+    let (source, result) = many0(block).context("basic_section_finder").parse(source)?;
+    let initial_source = &initial_source.replace(source, "");
+    Ok((
+        source,
+        Section::Basic {
+            attrs,
+            bounds: SectionBounds::Full,
+            content: result,
+            source: initial_source.to_string(),
+            r#type: r#type.to_string(),
+        },
+    ))
+}
--- cli/src/section/mod.rs
+++ cli/src/section/mod.rs
@@ -1,7 +1,9 @@
+pub mod basic;
 pub mod list;
 pub mod list_item;
 
 use crate::block::*;
+use crate::section::basic::basic_section;
 use crate::section::list::list_section;
 use crate::section_attr::*;
 use crate::span::empty_line;
@@ -13,7 +15,7 @@ use nom::character::complete::multispace0;
 use nom::character::complete::space0;
 use nom::character::complete::space1;
 use nom::combinator::eof;
-use nom::combinator::not;
+// use nom::combinator::not;
 use nom::combinator::rest;
 use nom::multi::many0;
 use std::collections::BTreeMap;
@@ -102,52 +104,6 @@ pub fn section<'a>(
     .parse(source)?;
     Ok((source, result))
 }
-
-fn basic_section<'a>(
-    source: &'a str,
-    list: &'a Vec<String>,
-) -> IResult<&'a str, Section, ErrorTree<&'a str>> {
-    let (source, result) = list.iter().fold(initial_error(), |acc, item| match acc {
-        Ok(v) => Ok(v),
-        _ => basic_section_finder(source, item),
-    })?;
-    Ok((source, result))
-}
-
-pub fn basic_section_finder<'a>(
-    source: &'a str,
-    key: &'a str,
-) -> IResult<&'a str, Section, ErrorTree<&'a str>> {
-    let initial_source = source;
-    let (source, _) = tag("--").context("basic_section_finder").parse(source)?;
-    let (source, _) = space1.context("basic_section_finder").parse(source)?;
-    let (source, r#type) = tag(key).context("basic_section_finder").parse(source)?;
-    let (source, _) = not(tag("/"))
-        .context("basic_section_finder")
-        .parse(source)?;
-    let (source, _) = alt((tuple((multispace0, eof)), tuple((space0, line_ending))))
-        .context("basic_section_finder")
-        .parse(source)?;
-    let (source, attrs) = many0(section_attr)
-        .context("basic_section_finder")
-        .parse(source)?;
-    let (source, _) = alt((empty_line.map(|_| ""), eof))
-        .context("basic_section_finder")
-        .parse(source)?;
-    let (source, result) = many0(block).context("basic_section_finder").parse(source)?;
-    let initial_source = &initial_source.replace(source, "");
-    Ok((
-        source,
-        Section::Basic {
-            attrs,
-            bounds: SectionBounds::Full,
-            content: result,
-            source: initial_source.to_string(),
-            r#type: r#type.to_string(),
-        },
-    ))
-}
-
 fn json_section<'a>(
     source: &'a str,
     list: &'a Vec<String>,
