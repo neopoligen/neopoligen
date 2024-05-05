a8c45a5a633f04ce0a7391ddb9c498b667ea79ae
Alan Smith <commit@alanwsmith.com>
2024-05-05 12:23:24 -0400

    set up raw sections

--- cli/src/section/raw.rs
+++ cli/src/section/raw.rs
@@ -24,7 +24,12 @@ pub fn raw_section<'a>(
 ) -> IResult<&'a str, Section, ErrorTree<&'a str>> {
     let (source, result) = list.iter().fold(initial_error(), |acc, item| match acc {
         Ok(v) => Ok(v),
-        _ => raw_full_section_finder(source, item),
+        _ => alt((
+            |src| raw_full_section_finder(src, item),
+            |src| raw_start_section_finder(src, item),
+        ))
+        .context("raw_sectio")
+        .parse(source),
     })?;
     Ok((source, result))
 }
@@ -58,6 +63,41 @@ fn raw_full_section_finder<'a>(
             attrs,
             bounds: SectionBounds::Full,
             text: text.to_string(),
+            source: initial_source.to_string(),
+            r#type: r#type.to_string(),
+        },
+    ))
+}
+
+fn raw_start_section_finder<'a>(
+    source: &'a str,
+    key: &'a str,
+) -> IResult<&'a str, Section, ErrorTree<&'a str>> {
+    let initial_source = source;
+    let (source, _) = tag("--").context("raw_full_section").parse(source)?;
+    let (source, _) = space1.context("raw_full_section").parse(source)?;
+    let (source, r#type) = tag(key).context("raw_full_section").parse(source)?;
+    let (source, _) = tag("/").context("raw_full_section").parse(source)?;
+    let (source, _) = tuple((space0, line_ending))
+        .context("raw_full_section")
+        .parse(source)?;
+    let (source, attrs) = many0(section_attr)
+        .context("raw_full_section")
+        .parse(source)?;
+    let (source, _) = alt((empty_line.map(|_| ""), eof))
+        .context("raw_full_section")
+        .parse(source)?;
+    let (source, text) = take_until("\n-- /")
+        .context("raw_full_section")
+        .parse(source)?;
+    let (source, _) = multispace0(source)?;
+    let initial_source = &initial_source.replace(source, "");
+    Ok((
+        source,
+        Section::Raw {
+            attrs,
+            bounds: SectionBounds::Start,
+            text: text.trim().to_string(),
             source: initial_source.to_string(),
             r#type: r#type.to_string(),
         },
--- cli/tests/section/raw.rs
+++ cli/tests/section/raw.rs
@@ -16,3 +16,18 @@ fn raw_full_section() {
     let right = section(&source, &config.sections).unwrap().1;
     assert_eq!(left, right);
 }
+
+#[test]
+fn raw_start_section() {
+    let source = "-- pre/\n\nfoxtrot victor\n\n-- /pre";
+    let config = SiteConfig::mock1();
+    let left = Section::Raw {
+        attrs: vec![],
+        bounds: SectionBounds::Start,
+        text: "foxtrot victor".to_string(),
+        source: "-- pre/\n\nfoxtrot victor\n\n".to_string(),
+        r#type: "pre".to_string(),
+    };
+    let right = section(&source, &config.sections).unwrap().1;
+    assert_eq!(left, right);
+}
