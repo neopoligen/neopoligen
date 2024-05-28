909467014a5d0ca404339aa72ee0208a550693b6
Alan Smith <commit@alanwsmith.com>
2024-05-27 22:22:23 -0400

    Checking file mod times for cache

--- cli/src/helpers/mod.rs
+++ cli/src/helpers/mod.rs
@@ -1,12 +1,28 @@
 use anyhow::Result;
 use regex::Regex;
+use std::fs;
 use std::path::PathBuf;
 
 pub fn cache_is_stale(source_path: &PathBuf, cache_path: &PathBuf) -> bool {
     if !cache_path.is_file() {
         true
     } else {
-        false
+        if let (Ok(source_data), Ok(cache_data)) =
+            (fs::metadata(&source_path), fs::metadata(&cache_path))
+        {
+            if let (Ok(source_mod), Ok(cache_mod)) = (source_data.modified(), cache_data.modified())
+            {
+                if source_mod > cache_mod {
+                    true
+                } else {
+                    false
+                }
+            } else {
+                true
+            }
+        } else {
+            true
+        }
     }
 }
 
