38e5f56b7a7f958ad4648b843bea14b5d0270957
Alan Smith <commit@alanwsmith.com>
2024-05-31 13:44:03 -0400

    tested two attrs in yaml

--- cli/tests/section_v39/yaml.rs
+++ cli/tests/section_v39/yaml.rs
@@ -6,16 +6,24 @@ use pretty_assertions::assert_eq;
 #[test]
 fn yaml_basic() {
     let config = SiteConfig::mock1();
-    let source = "-- metadata\n-- id: alfa1234\n";
+    let source = "-- metadata\n-- id: alfa1234\n-- created: 2024-05-31T13:43:03-04:00";
     let left = (
         "",
         SectionV39 {
-            attrs: vec![SectionAttrV39 {
-                kind: SectionAttrV39Kind::KeyValue {
-                    key: "id".to_string(),
-                    value: "alfa1234".to_string(),
+            attrs: vec![
+                SectionAttrV39 {
+                    kind: SectionAttrV39Kind::KeyValue {
+                        key: "id".to_string(),
+                        value: "alfa1234".to_string(),
+                    },
                 },
-            }],
+                SectionAttrV39 {
+                    kind: SectionAttrV39Kind::KeyValue {
+                        key: "created".to_string(),
+                        value: "2024-05-31T13:43:03-04:00".to_string(),
+                    },
+                },
+            ],
             bounds: SectionV39Bounds::Full,
             kind: SectionV39Kind::Yaml {},
             r#type: "metadata".to_string(),
