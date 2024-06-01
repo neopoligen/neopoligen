33c29f15eb33b9d902aec8f2cb57f72a736f4773
Alan Smith <commit@alanwsmith.com>
2024-06-01 11:37:15 -0400

    But in basic full section test for attrs

--- cli/tests/section_v39/basic.rs
+++ cli/tests/section_v39/basic.rs
@@ -1,4 +1,5 @@
 use neopoligengine::{
+    section_attr_v39::{SectionAttrV39, SectionAttrV39Kind},
     section_v39::*,
     site_config::SiteConfig,
     span_v39::{SpanV39, SpanV39Kind},
@@ -6,7 +7,7 @@ use neopoligengine::{
 use pretty_assertions::assert_eq;
 
 #[test]
-fn basic_section_basic_test() {
+fn basic_section_full_core_test() {
     let config = SiteConfig::mock1();
     let source = "-- title\n\nHello World";
     let left = (
@@ -41,6 +42,53 @@ fn basic_section_basic_test() {
                 }],
             },
             r#type: "title".to_string(),
+        },
+    );
+    let right = start_or_full_section_v39(source, &config.sections, &config.spans).unwrap();
+    assert_eq!(left, right);
+}
+
+#[test]
+fn basic_section_full_with_attrs() {
+    let config = SiteConfig::mock1();
+    let source = "-- div\n-- class: green\n\nalfa bravo";
+    let left = (
+        "",
+        SectionV39 {
+            attrs: vec![SectionAttrV39 {
+                kind: SectionAttrV39Kind::KeyValue {
+                    key: "class".to_string(),
+                    value: "green".to_string(),
+                },
+            }],
+            bounds: SectionV39Bounds::Full,
+            kind: SectionV39Kind::Basic {
+                children: vec![SectionV39 {
+                    attrs: vec![],
+                    bounds: SectionV39Bounds::Full,
+                    kind: SectionV39Kind::Block {
+                        spans: vec![
+                            SpanV39 {
+                                kind: SpanV39Kind::WordPart {
+                                    text: "alfa".to_string(),
+                                },
+                            },
+                            SpanV39 {
+                                kind: SpanV39Kind::Space {
+                                    text: " ".to_string(),
+                                },
+                            },
+                            SpanV39 {
+                                kind: SpanV39Kind::WordPart {
+                                    text: "bravo".to_string(),
+                                },
+                            },
+                        ],
+                    },
+                    r#type: "block-of-text".to_string(),
+                }],
+            },
+            r#type: "div".to_string(),
         },
     );
     let right = start_or_full_section_v39(source, &config.sections, &config.spans).unwrap();
