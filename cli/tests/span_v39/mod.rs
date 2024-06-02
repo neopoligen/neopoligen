63199bf79cc696d36bb4030d045735a85e8e0ec3
Alan Smith <commit@alanwsmith.com>
2024-06-01 23:22:18 -0400

    Put in tests for strucute newline with leading spaces or eof

--- cli/tests/span_v39/mod.rs
+++ cli/tests/span_v39/mod.rs
@@ -1,21 +1,29 @@
 use neopoligengine::{site_config::SiteConfig, span_v39::*};
 use pretty_assertions::assert_eq;
 
-// #[test]
-// fn line_ending_or_eof_with_line_ending() {
-//     let source = "\n";
-//     let left = ("", "\n");
-//     let right = format_structure_line_ending_or_eof(source).unwrap();
-//     assert_eq!(left, right);
-// }
+#[test]
+fn structure_empty_until_newline_or_eof_basic() {
+    let source = "\n";
+    let left = ("", "");
+    let right = structure_empty_until_newline_or_eof(source).unwrap();
+    assert_eq!(left, right);
+}
 
-// #[test]
-// fn line_ending_or_eof_with_eof() {
-//     let source = "";
-//     let left = ("", "");
-//     let right = format_structure_line_ending_or_eof(source).unwrap();
-//     assert_eq!(left, right);
-// }
+#[test]
+fn structure_empty_until_newline_or_eof_with_leading_spaces() {
+    let source = "   \n";
+    let left = ("", "");
+    let right = structure_empty_until_newline_or_eof(source).unwrap();
+    assert_eq!(left, right);
+}
+
+#[test]
+fn structure_empty_until_newline_or_eof_at_eof() {
+    let source = "";
+    let left = ("", "");
+    let right = structure_empty_until_newline_or_eof(source).unwrap();
+    assert_eq!(left, right);
+}
 
 #[test]
 fn space_basic() {
