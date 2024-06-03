7110ff88f958e57f74971d334e610d3a2fcdc2ac
Alan Smith <commit@alanwsmith.com>
2024-06-03 12:43:27 -0400

    Loading site config into builder from engine config

--- cli/src/builder/mod.rs
+++ cli/src/builder/mod.rs
@@ -1,6 +1,9 @@
+use std::fs;
+
 use crate::{engine_config::EngineConfig, site_config::SiteConfig};
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
+use serde_json;
 use tracing::{event, instrument, Level};
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
@@ -10,7 +13,17 @@ pub struct Builder {
 
 impl Builder {
     pub fn new_from_engine_config(engine_config: &EngineConfig) -> Result<Builder> {
-        let b = Builder { config: None };
+        let project_root = engine_config
+            .sites_dir
+            .join(engine_config.active_site.as_str());
+        let config_path = project_root.join("admin").join("config.json");
+        let text = fs::read_to_string(config_path)?;
+        let mut config = serde_json::from_str::<SiteConfig>(&text)?;
+        config.project_root = Some(project_root);
+        config.load_sections();
+        let b = Builder {
+            config: Some(config),
+        };
         Ok(b)
     }
 }
