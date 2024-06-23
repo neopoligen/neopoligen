5fd60ca9605305aea46fb1b6e3ed85fd1589bfc2
Alan Smith <commit@alanwsmith.com>
2024-06-23 11:48:17 -0400

    Added list end section parser

--- cli/src/section/list.rs
+++ cli/src/section/list.rs
@@ -3,6 +3,7 @@ use crate::section::*;
 use crate::section_attr::*;
 use crate::span::*;
 // use nom::branch::alt;
+use crate::section::block::block_of_end_content;
 use nom::bytes::complete::tag;
 use nom::character::complete::multispace0;
 use nom::multi::many0;
@@ -10,6 +11,33 @@ use nom::IResult;
 use nom::Parser;
 use nom_supreme::error::ErrorTree;
 use nom_supreme::parser_ext::ParserExt;
+
+pub fn list_section_end<'a>(
+    source: &'a str,
+    key: &'a str,
+) -> IResult<&'a str, Section, ErrorTree<&'a str>> {
+    let (source, _) = tag("-- ").context("").parse(source)?;
+    let (source, _) = tag("/").context("").parse(source)?;
+    let (source, r#type) = tag(key).context("").parse(source)?;
+    let (source, _) = structure_empty_until_newline_or_eof
+        .context("")
+        .parse(source)?;
+    let (source, attrs) = many0(section_attr).context("").parse(source)?;
+    let (source, _) = structure_empty_until_newline_or_eof
+        .context("")
+        .parse(source)?;
+    let (source, _) = multispace0.context("").parse(source)?;
+    let (source, children) = many0(|src| block_of_end_content(src))
+        .context("")
+        .parse(source)?;
+    let section = Section {
+        attrs,
+        bounds: SectionBounds::End,
+        kind: SectionKind::List { children },
+        r#type: r#type.to_string(),
+    };
+    Ok((source, section))
+}
 
 pub fn list_section_full<'a>(
     source: &'a str,
@@ -35,6 +63,37 @@ pub fn list_section_full<'a>(
         Section {
             attrs,
             bounds: SectionBounds::Full,
+            kind: SectionKind::List { children },
+            r#type: r#type.to_string(),
+        },
+    ))
+}
+
+pub fn list_section_start<'a>(
+    source: &'a str,
+    sections: &'a ConfigSections,
+) -> IResult<&'a str, Section, ErrorTree<&'a str>> {
+    let (source, _) = tag("-- ").context("").parse(source)?;
+    let (source, r#type) = (|src| tag_finder(src, &sections.list))
+        .context("")
+        .parse(source)?;
+    let (source, _) = tag("/").context("").parse(source)?;
+    let (source, _) = structure_empty_until_newline_or_eof
+        .context("")
+        .parse(source)?;
+    let (source, attrs) = many0(section_attr).context("").parse(source)?;
+    let (source, _) = structure_empty_until_newline_or_eof
+        .context("")
+        .parse(source)?;
+    let (source, _) = multispace0.context("").parse(source)?;
+    let (source, children) = many1(|src| list_item_full(src, sections))
+        .context("")
+        .parse(source)?;
+    Ok((
+        source,
+        Section {
+            attrs,
+            bounds: SectionBounds::Start,
             kind: SectionKind::List { children },
             r#type: r#type.to_string(),
         },
@@ -80,6 +139,61 @@ mod test {
             },
         );
         let right = list_section_full(source, &config.sections).unwrap();
+        assert_eq!(left, right);
+    }
+
+    #[test]
+    #[ignore]
+    fn solo_basic_start_end_list_test() {
+        let source = "-- list/\n\n- alfa\n\n-- /list";
+        let config = SiteConfig::mock1_basic();
+        let left = (
+            "",
+            Section {
+                attrs: vec![],
+                bounds: SectionBounds::Start,
+                kind: SectionKind::List {
+                    children: vec![Section {
+                        attrs: vec![],
+                        bounds: SectionBounds::Full,
+                        kind: SectionKind::ListItem {
+                            children: vec![Section {
+                                attrs: vec![],
+                                bounds: SectionBounds::Full,
+                                kind: SectionKind::Block {
+                                    spans: vec![Span {
+                                        attrs: vec![],
+                                        kind: SpanKind::WordPart,
+                                        parsed_text: "alfa".to_string(),
+                                    }],
+                                },
+                                r#type: "block-of-text".to_string(),
+                            }],
+                        },
+                        r#type: "list-item".to_string(),
+                    }],
+                },
+                r#type: "list".to_string(),
+            },
+        );
+        let right = list_section_start(source, &config.sections).unwrap();
+        assert_eq!(left, right);
+    }
+
+    #[test]
+    fn solo_basic_end_list_test() {
+        let source = "-- /list";
+        let config = SiteConfig::mock1_basic();
+        let left = (
+            "",
+            Section {
+                attrs: vec![],
+                bounds: SectionBounds::End,
+                kind: SectionKind::List { children: vec![] },
+                r#type: "list".to_string(),
+            },
+        );
+        let right = list_section_end(source, "list").unwrap();
         assert_eq!(left, right);
     }
 
